b858f0b5df8853546fe0c414da65c0c94f45d373
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-28 19:53:01 +0300

    add details panel to Confirm/Rejqct admin func

--- scripts/adminPanel.js
+++ scripts/adminPanel.js
@@ -3,6 +3,7 @@ function adminPanelBtnClick(){
     const adminPanelZone = document.getElementById('adminPanelZone');
     const adminPanelOpenButton = document.getElementById('adminPanelOpenButton');
     const closeBtnAdminPanelZone = document.getElementById('closeBtnAdminPanelZone');
+    const closeBtnAdminPaneDetails = document.getElementById('closeBtnAdminPaneDetails');
 
 
     adminPanelOpenButton.addEventListener('click', (e) => {
@@ -13,6 +14,11 @@ function adminPanelBtnClick(){
     closeBtnAdminPanelZone.addEventListener('click', (e) => {
         adminPanelZone.style = 'display: none';
         location.reload();//Todo временный костыль чтобы добавить новые подтвержденные события на карту
+    });
+
+    closeBtnAdminPaneDetails.addEventListener('click', (e) => {
+        const adminPaneDetails = document.getElementById('adminPaneDetails');
+        adminPaneDetails.style = 'display: none';
     });
 }
 
@@ -37,7 +43,7 @@ function addUnconfirmedEvents(){
                 createEventShortDescriptionDiv(newEventItemDiv, unconfirmedEvent.eventDto.category);
                 createEventShortDescriptionDiv(newEventItemDiv, unconfirmedEvent.userDto.login);
                 createEventShortDescriptionDiv(newEventItemDiv, unconfirmedEvent.userDto.roleName);
-                addConfirmAndRejectShortDescriptionDiv(newEventItemDiv, unconfirmedEvent.eventDto.id, rootElement);
+                addConfirmRejectAndDetailsShortDescriptionDiv(newEventItemDiv, unconfirmedEvent.eventDto.id, rootElement);
 
                 rootElement.append(newEventItemDiv);
             }
@@ -69,7 +75,7 @@ function createEventShortDescriptionDiv(root, divText){
     root.append(shortDescriptionDiv);
 }
 
-function addConfirmAndRejectShortDescriptionDiv(root, eventId, adminPanelMain){
+function addConfirmRejectAndDetailsShortDescriptionDiv(root, eventId, adminPanelMain){
     var confirmDiv = document.createElement("div");
     confirmDiv.className = "event-short-description event-confirm";
     confirmDiv.innerHTML = "Confirm";
@@ -80,6 +86,11 @@ function addConfirmAndRejectShortDescriptionDiv(root, eventId, adminPanelMain){
     rejectDiv.innerHTML = "Reject";
     root.append(rejectDiv);
 
+    var detailsDiv = document.createElement("div");
+    detailsDiv.className = "event-short-description event-details";
+    detailsDiv.innerHTML = "Details";
+    root.append(detailsDiv);
+
     var confirmOrRejectData = 
     {
         "eventId": eventId
@@ -87,7 +98,6 @@ function addConfirmAndRejectShortDescriptionDiv(root, eventId, adminPanelMain){
     confirmOrRejectData = JSON.stringify(confirmOrRejectData);
 
     confirmDiv.addEventListener('click', (e) => {
-        
         $.ajax({
             url: `https://interactivenaturaldisastermapapi.azurewebsites.net/api/UnconfirmedEvent/confirm/${eventId}`,
             method: 'PATCH',
@@ -108,7 +118,6 @@ function addConfirmAndRejectShortDescriptionDiv(root, eventId, adminPanelMain){
     });
 
     rejectDiv.addEventListener('click', (e) => {
-        
         $.ajax({
             url: `https://interactivenaturaldisastermapapi.azurewebsites.net/api/UnconfirmedEvent/reject/${eventId}`,
             method: 'PATCH',
@@ -126,6 +135,126 @@ function addConfirmAndRejectShortDescriptionDiv(root, eventId, adminPanelMain){
                 exceptionHandler(err);
             }
         });
+    });
+
+    detailsDiv.addEventListener('click', (e) => {
+        const adminPaneDetails = document.getElementById('adminPaneDetails');
+        adminPaneDetails.style = '';
+
+        const closeBtnAdminPaneDetails = document.getElementById('closeBtnAdminPaneDetails');
+        adminPaneDetails.replaceChildren();
+        adminPaneDetails.append(closeBtnAdminPaneDetails);
+
+        var eventDetailsItem = document.createElement("div");
+        eventDetailsItem.className = "details-item";
+        var div = document.createElement("div");
+        div.innerHTML = "Event Info";
+        eventDetailsItem.append(div);
+
+        var userDetailsItem = document.createElement("div");
+        userDetailsItem.className = "details-item";
+        var div = document.createElement("div");
+        div.innerHTML = "User Info";
+        userDetailsItem.append(div);
+
+        $.ajax({
+            url: `https://interactivenaturaldisastermapapi.azurewebsites.net/api/UnconfirmedEvent/${eventId}`,
+            method: 'GET',
+            contentType: "application/json; charset=utf-8",
+            headers: {
+                'Authorization':`bearer ${localStorage.getItem('jwt')}`
+            },
+            success: function(data){
+                div = document.createElement("div");
+                div.innerHTML = "Id: " + data.eventDto.id;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Title: " + data.eventDto.title;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Link: " + data.eventDto.link;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Start Date: " + data.eventDto.startDate;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "End Date: " + data.eventDto.endDate;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Closed: " + data.eventDto.closed;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Magnitude Value: " + data.eventDto.magnitudeValue;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Confirmed: " + data.eventDto.confirmed;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Category: " + data.eventDto.category;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Source: " + data.eventDto.source;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Magnitude Unit: " + data.eventDto.magnitudeUnit;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Latitude: " + data.eventDto.latitude;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Longitude: " + data.eventDto.longitude;
+                eventDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Event Hazard Unit: " + data.eventDto.eventHazardUnit;
+                eventDetailsItem.append(div);
+
+
+                div = document.createElement("div");
+                div.innerHTML = "Id: " + data.userDto.id;
+                userDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "First Name: " + data.userDto.firstName;
+                userDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Email: " + data.userDto.email;
+                userDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Telegram: " + data.userDto.telegram;
+                userDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Login: " + data.userDto.login;
+                userDetailsItem.append(div);
+
+                div = document.createElement("div");
+                div.innerHTML = "Role: " + data.userDto.roleName;
+                userDetailsItem.append(div);
+            },
+            error: function(jqXHR, textStatus, error) {
+                var err = textStatus + " " + jqXHR.status + ", " + error + "\n"
+                        + jqXHR.responseText.toString();
+                exceptionHandler(err);
+            }
+        });
+
+        adminPaneDetails.append(eventDetailsItem);
+        adminPaneDetails.append(userDetailsItem);
     });
 }
 
